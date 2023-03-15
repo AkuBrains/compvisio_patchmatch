dfac5b57983aa6012b412abb513d47132dd27089
Selman Sezgin <selman.szg@gmail.com>
2023-03-15 11:00:21 +0100

    Hyprid integration added

--- integration.py
+++ integration.py
@@ -8,6 +8,7 @@ def get_masks_direct(img_ref, mask_ref, imgs, n_iter_mc, p_size, n_iter_pm, thr=
     n_images = len(imgs)
     estimated_masks = np.zeros((n_images, n, m))
     for i in range(n_images):
+<<<<<<< HEAD
         print("Mask estimation for ",i)
         f_monte_carlo = monte_carlo(imgs[i], img_ref, n_iter=n_iter_mc, p_size=p_size, pm_iter=n_iter_pm)
         for j in range(n_iter_mc):
@@ -16,13 +17,27 @@ def get_masks_direct(img_ref, mask_ref, imgs, n_iter_mc, p_size, n_iter_pm, thr=
         estimated_masks[i] /= n_iter_mc
         estimated_masks[i][estimated_masks[i] < thr] = 0
     return estimated_masks.astype(np.int32)
+=======
+        print(f"Mask estimation for {name+'-%0*d.bmp'%(3, i+2)}")
+        f_monte_carlo = monte_carlo(imgs[i], img_ref, n_iter=n_iter_mc, p_size=p_size, pm_iter=n_iter_pm)
+        for j in range(n_iter_mc):
+            mask_i = estimate_mask(mask_ref, f_monte_carlo[j])
+            estimated_masks[i] += mask_i
+        estimated_masks[i] /= n_iter_mc
+        estimated_masks[i][estimated_masks[i] < thr] = 0
+    return estimated_masks
+>>>>>>> e91941d (Hyprid integration added)
 
 def get_masks_sequential(img_ref, mask_ref, imgs, n_iter_mc, p_size, n_iter_pm, thr=100):
     n, m = img_ref.shape[0], img_ref.shape[1] # Dimension of the images/masks
     n_images = len(imgs)
     estimated_masks = np.zeros((n_images, n, m))
     for i in range(n_images):
+<<<<<<< HEAD
         print("Mask estimation for ",i)
+=======
+        print(f"Mask estimation for {name+'-%0*d.bmp'%(3, i+2)}")
+>>>>>>> e91941d (Hyprid integration added)
         f_monte_carlo = monte_carlo(imgs[i], img_ref, n_iter=n_iter_mc, p_size=p_size, pm_iter=n_iter_pm)
         for j in range(n_iter_mc):
             mask_i = estimate_mask(mask_ref, f_monte_carlo[j,...])
@@ -31,22 +46,38 @@ def get_masks_sequential(img_ref, mask_ref, imgs, n_iter_mc, p_size, n_iter_pm, 
         estimated_masks[i][estimated_masks[i] < thr] = 0
         img_ref = np.copy(imgs[i])
         mask_ref = np.copy(estimated_masks[i])
+<<<<<<< HEAD
     return estimated_masks.astype(np.int32)
+=======
+    return estimated_masks
+>>>>>>> e91941d (Hyprid integration added)
 
 def get_masks_hybrid(img_ref, mask_ref, imgs, n_iter_mc, p_size, n_iter_pm, step = 5, thr=100):
     n, m = img_ref.shape[0], img_ref.shape[1] # Dimension of the images/masks
     n_images = len(imgs)
     estimated_masks = np.zeros((n_images, n, m))
     for i in range(n_images):
+<<<<<<< HEAD
         print("Mask estimation for ",i)
+=======
+        print(f"Mask estimation for {name+'-%0*d.bmp'%(3, i+2)}")
+>>>>>>> e91941d (Hyprid integration added)
         f_monte_carlo = monte_carlo(imgs[i], img_ref, n_iter=n_iter_mc, p_size=p_size, pm_iter=n_iter_pm)
         for j in range(n_iter_mc):
             mask_i = estimate_mask(mask_ref, f_monte_carlo[j,...])
             estimated_masks[i,...] += mask_i
         estimated_masks[i] /= n_iter_mc
         estimated_masks[i][estimated_masks[i] < thr] = 0
+<<<<<<< HEAD
+=======
+
+>>>>>>> e91941d (Hyprid integration added)
         if i%step==0 and i!=0:
             img_ref = np.copy(imgs[i])
             mask_ref = np.copy(estimated_masks[i])
 
+<<<<<<< HEAD
     return estimated_masks.astype(np.int32)
+=======
+    return estimated_masks
+>>>>>>> e91941d (Hyprid integration added)
